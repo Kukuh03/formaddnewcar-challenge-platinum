5fcd7be6be3e81ada17a98906c6ddab7406380c7
Kukuh Ferlanda <kukuh.ferlanda@bisnis.com>
2022-09-23 15:42:23 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Formaddnewcar from './components/Formaddnewcar';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Formaddnewcar />
+    </div>
+  );
+}
+
+export default App;
